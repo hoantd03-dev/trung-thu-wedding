f38b859e2e95c5fa596c38ccefd5018c2428e13a
Hoan Tran <hoantd03@gmail.com>
2026-02-25 17:24:47 +0700

    update

--- js/custom.js
+++ js/custom.js
@@ -121,29 +121,36 @@
     Countdown Clock
     ................................................. */
 	function makeTimer() {
-		var endTime = new Date("01 June 2019 10:45:00 GMT+01:00");
-		endTime = (Date.parse(endTime) / 1000);
 
-		var now = new Date();
-		now = (Date.parse(now) / 1000);
+		// 05 April 2026 - 00:00:00 giờ Việt Nam (GMT+7)
+		var endTime = new Date("2026-04-05T00:00:00+07:00");
+		endTime = endTime.getTime();
+
+		var now = new Date().getTime();
 
 		var timeLeft = endTime - now;
 
-		var days = Math.floor(timeLeft / 86400);
-		var hours = Math.floor((timeLeft - (days * 86400)) / 3600);
-		var minutes = Math.floor((timeLeft - (days * 86400) - (hours * 3600)) / 60);
-		var seconds = Math.floor((timeLeft - (days * 86400) - (hours * 3600) - (minutes * 60)));
+		if (timeLeft < 0) {
+			timeLeft = 0;
+		}
 
-		if (hours < "10") { hours = "0" + hours; }
-		if (minutes < "10") { minutes = "0" + minutes; }
-		if (seconds < "10") { seconds = "0" + seconds; }
+		var days = Math.floor(timeLeft / (1000 * 60 * 60 * 24));
+		var hours = Math.floor((timeLeft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+		var minutes = Math.floor((timeLeft % (1000 * 60 * 60)) / (1000 * 60));
+		var seconds = Math.floor((timeLeft % (1000 * 60)) / 1000);
 
-		$("#days").html(days + "<h6>Days</h6>");
-		$("#hours").html(hours + "<h6>Hrs</h6>");
-		$("#minutes").html(minutes + "<h6>Min</h6>");
-		$("#seconds").html(seconds + "<h6>Sec</h6>");
+		if (hours < 10) { hours = "0" + hours; }
+		if (minutes < 10) { minutes = "0" + minutes; }
+		if (seconds < 10) { seconds = "0" + seconds; }
+
+		$("#days").html(days + "<h6>Ngày</h6>");
+		$("#hours").html(hours + "<h6>Giờ</h6>");
+		$("#minutes").html(minutes + "<h6>Phút</h6>");
+		$("#seconds").html(seconds + "<h6>Giây</h6>");
 	}
-	setInterval(function () { makeTimer(); }, 1000);
+
+	setInterval(makeTimer, 1000);
+	makeTimer();
 
 
 
